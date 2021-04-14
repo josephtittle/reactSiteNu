05a3e1acdad9b1ce45b68c2cbb1e34fc6565f4a4
Joseph Tittle <josephtittle91@gmail.com>
2021-04-13 21:58:55 -0500

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -30,18 +30,18 @@ class CampsiteInfo extends Component {
             <div/>
             )
     }
-    render(){
-    if(this.props.campsite){
-        return(
-            <div className="row">
-                {this.renderCampsite(this.props.campsite)}
-                {this.renderComments(this.props.campsite.comments)}
-            </div>
-        )
-    }
-    return(
-        <div/>
-    )
+    render() {
+        if (this.props.campsite) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                </div>
+            );
+        }
+        return <div />;
     }
 }
 export default CampsiteInfo;
